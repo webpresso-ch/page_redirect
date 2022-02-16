95b6bb662e36a4dc637f667a8a8751c2f883f835
juescheWeb <77046483+juescheWeb@users.noreply.github.com>
2022-02-16 07:53:38 +0100

    check if the page exists

--- src/PageRedirect.php
+++ src/PageRedirect.php
@@ -6,6 +6,7 @@ defined('C5_EXECUTE') or die("Access Denied.");
 
 use Loader;
 use Page;
+
 /**
  * Class that is used to redirect based on a page attribute
  * @package Page Redirect
@@ -22,21 +23,23 @@ class PageRedirect
     {
         $page = Page::getCurrentPage();
         //get attribute
-        $page_selector = $page->getCollectionAttributeValue('page_selector_redirect');
-        //start checking if its a valid page
-        if ($page_selector > 0) {
-            Loader::model('page');
-            $npage = Page::getByID($page_selector);
-            //more checking
-            if (is_object($npage) && !$npage->isError()) {
-                //redirect
-                header("HTTP/1.1 301 Moved Permanently");
-                if (!$npage->isExternalLink()) {
-                    $nh = Loader::Helper('navigation');
-                    header('Location: ' . $nh->getLinkToCollection($npage, true));
-                } else {
-                    header('Location: ' . $npage->getCollectionPointerExternalLink());
-                    exit;
+        if ($page) {
+            $page_selector = $page->getCollectionAttributeValue('page_selector_redirect');
+            //start checking if its a valid page
+            if ($page_selector > 0) {
+                Loader::model('page');
+                $npage = Page::getByID($page_selector);
+                //more checking
+                if (is_object($npage) && !$npage->isError()) {
+                    //redirect
+                    header("HTTP/1.1 301 Moved Permanently");
+                    if (!$npage->isExternalLink()) {
+                        $nh = Loader::Helper('navigation');
+                        header('Location: ' . $nh->getLinkToCollection($npage, true));
+                    } else {
+                        header('Location: ' . $npage->getCollectionPointerExternalLink());
+                        exit;
+                    }
                 }
             }
         }
